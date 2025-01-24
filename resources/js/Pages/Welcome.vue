dfa2f921e817e1e123e6513cc5b26cd5efd77eeb
Kim-CheolHee <windowlife92@gmail.com>
2025-01-24 22:36:18 +0700

    대시보드에 시계 nav 링크 추가가

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -65,6 +65,13 @@ function handleImageError() {
 
                         <template v-else>
                             <Link
+                                :href="route('world_time')"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                            >
+                                World time
+                            </Link>
+
+                            <Link
                                 :href="route('login')"
                                 class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
